ebf158ff78040978cc44387a6ee546fdff7a11f5
Haoyu Wang <167748030+hywang2002@users.noreply.github.com>
2024-05-03 20:23:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,19 +12,4 @@ CUDA_VISIBLE_DEVICES=3 python test.py --gpu_id 0 \
 
 #!/bin/bash
 
-# # 循环遍历变量 i 从 0 到 40
-# for ((i=2; i<10; i++)); do
-#     CUDA_VISIBLE_DEVICES=6 python test.py --gpu_id 0 \
-#     --ddim_steps 50 \
-#     --outdir results/dci-vton-local-mv-unpaired/ep$i \
-#     --config configs/viton512.yaml \
-#     --dataroot /mnt/pfs-mc0p4k/cvg/team/didonglin/why/datasets/mv_1000_split \
-#     --ckpt /mnt/pfs-mc0p4k/cvg/team/didonglin/why/DCI-VTON-Virtual-Try-On-skip/models/dci-vton-mv1000-only-local/2024-04-15T14-28-50_viton512/checkpoints/epoch=00000$i.ckpt \
-#     --n_samples 1 \
-#     --seed 23 \
-#     --scale 1 \
-#     --H 512 \
-#     --W 384 
-# done
-
 # code
