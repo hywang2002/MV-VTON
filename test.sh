960c4234ae525bbb0594439e42b385c9c23ad819
“hywang2002” <cshy2002@gmail.com>
2024-05-16 16:44:02 +0800

    test1

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=3 python test.py --gpu_id 0 \
 --outdir results/dci-vton-all/2k/ \
 --config configs/viton512.yaml \
 --dataroot /mnt/pfs-mc0p4k/cvg/team/didonglin/why/datasets/mv_1000_split \
---ckpt /mnt/pfs-mc0p4k/cvg/team/didonglin/why/DCI-VTON-Virtual-Try-On-skip/models/dci-vton-mv1000-warp-code-wo-res-zero-skip-softmax/2024-04-10T11-49-55_viton512/checkpoints/epoch=000006.ckpt \
+--ckpt /mnt/pfs-mc0p4k/cvg/team/didonglin/why/MV-VTON-ori/checkpoints/mvg.ckpt  \
 --n_samples 1 \
 --seed 23 \
 --scale 1 \
